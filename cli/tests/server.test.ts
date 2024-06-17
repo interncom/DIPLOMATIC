772a8f962425ef09f7346f71358fe54ec7b219ba
Mason <86958+masonicboom@users.noreply.github.com>
2024-06-17 16:42:13 -0700

    refactor endpoint tests

--- cli/tests/server.test.ts
+++ cli/tests/server.test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { IRegistrationRequest } from "../src/types.ts";
+import type { IRegistrationRequest } from "../src/types.ts";
 import { encode } from "https://deno.land/x/msgpack@v1.4/mod.ts";
 
 function extractUrl(str: string): string | null {
@@ -39,47 +39,41 @@ async function startServer(): Promise<{ proc: Deno.Process, url: URL } | undefin
   }
 }
 
-Deno.test("GET /id", async () => {
-  const server = await startServer();
-  if (!server) {
-    throw "a fit";
-  }
-
-  try {
-    const url = server.url;
-    url.pathname = "/id";
-    const response = await fetch(url, { method: "GET" });
-    const body = await response.text();
-    assertEquals(body, serverID);
-  } finally {
-    server.proc.close();
-    server.proc.stdout?.close();
-    server.proc.stderr?.close();
-  }
-});
-
-
-Deno.test("POST /users", async () => {
-  const server = await startServer();
-  if (!server) {
-    throw "a fit";
-  }
-
-  try {
-    const req: IRegistrationRequest = {
-      token: registrationToken,
-      pubKey: new Uint8Array([0xFF]),
-    };
-    const reqPack = encode(req);
+function testEndpoint(pathname: string, testFunc: (url: URL) => Promise<void>): () => Promise<void> {
+  return async () => {
+    const server = await startServer();
+    if (!server) {
+      throw "a fit";
+    }
 
     const url = server.url;
-    url.pathname = "/users";
-    const response = await fetch(url, { method: "POST", body: reqPack });
-    await response.body?.cancel()
-    assertEquals(response.status, 200);
-  } finally {
-    server.proc.close();
-    server.proc.stdout?.close();
-    server.proc.stderr?.close();
+    url.pathname = pathname;
+
+    try {
+      await testFunc(url);
+    } finally {
+      server.proc.close();
+      server.proc.stdout?.close();
+      server.proc.stderr?.close();
+    }
   }
-});
+}
+
+Deno.test("GET /id", testEndpoint("/id", async (url) => {
+  const response = await fetch(url, { method: "GET" });
+  const body = await response.text();
+  assertEquals(body, serverID);
+}));
+
+
+Deno.test("POST /users", testEndpoint("/users", async (url) => {
+  const req: IRegistrationRequest = {
+    token: registrationToken,
+    pubKey: new Uint8Array([0xFF]), // TODO: check for valid pubKey length, server-side.
+  };
+  const reqPack = encode(req);
+
+  const response = await fetch(url, { method: "POST", body: reqPack });
+  await response.body?.cancel()
+  assertEquals(response.status, 200);
+}));

ef21108b56c8cb09c3fe5dd4839ec89d258e6137
Mason <86958+masonicboom@users.noreply.github.com>
2024-06-17 19:40:52 -0700

    test POST /ops

--- cli/src/server.ts
+++ cli/src/server.ts
@@ -2,7 +2,9 @@
 
 import { port } from "./consts.ts";
 import { decodeAsync } from "https://deno.land/x/msgpack@v1.4/mod.ts";
-import type { IRegistrationRequest } from "./types.ts";
+import type { IOperationRequest, IRegistrationRequest } from "./types.ts";
+import { checkSig } from "./auth.ts";
+import { btoh, htob } from "./lib.ts";
 
 interface IStoredOp {
   bin: Uint8Array;
@@ -10,12 +12,12 @@ interface IStoredOp {
 }
 
 interface IStorage {
-  users: unknown;
+  users: Set<string>;
   ops: IStoredOp[];
 }
 
 const storage: IStorage = {
-  users: {}, // Map from userID to signing keys.
+  users: new Set<string>(), // Set of user pubkeys in hex.
   ops: [],
 }
 
@@ -43,13 +45,55 @@ const handler = async (request: Request): Promise<Response> => {
       if (!request.body) {
         return new Response("Invalid request", { status: 400 });
       }
-      const regReq = await decodeAsync(request.body) as IRegistrationRequest;
-      if (regReq.token === undefined || regReq.pubKey === undefined) {
+      const req = await decodeAsync(request.body) as IRegistrationRequest;
+      if (req.token === undefined || req.pubKey === undefined) {
         return new Response("Invalid request", { status: 400 });
       }
-      if (regReq.token !== regToken) {
+      if (req.token !== regToken) {
         return new Response("Unauthorized", { status: 401 });
       }
+      // TODO: check pubKey length.
+      const pubKeyHex = btoh(req.pubKey);
+      storage.users.add(pubKeyHex);
+      return new Response(null, { status: 200 });
+    } catch {
+      return new Response("Processing request", { status: 500 });
+    }
+  }
+
+  if (request.method === "POST" && url.pathname === "/ops") {
+    try {
+      if (!request.body) {
+        return new Response("Invalid request", { status: 400 });
+      }
+      const req = await decodeAsync(request.body) as IOperationRequest;
+      if (req.cipher === undefined) {
+        return new Response("Invalid request", { status: 400 });
+      }
+
+      // Check user is registerd.
+      const pubKeyHex = request.headers.get("X-DIPLOMATIC-KEY");
+      if (!pubKeyHex) {
+        return new Response("Missing pubkey", { status: 401 });
+      }
+      if (!storage.users.has(pubKeyHex)) {
+        return new Response("Unauthorized", { status: 401 });
+      }
+
+      // Check signature.
+      const sigHex = request.headers.get("X-DIPLOMATIC-SIG");
+      if (!sigHex) {
+        return new Response("Missing signature", { status: 401 });
+      }
+      const pubKey = htob(pubKeyHex);
+      const sig = htob(sigHex);
+      const sigValid = checkSig(sig, req.cipher, pubKey);
+      if (!sigValid) {
+        throw "Invalid signature";
+      }
+
+      // TODO: Store encrypted op.
+
       return new Response(null, { status: 200 });
     } catch {
       return new Response("Processing request", { status: 500 });
--- cli/tests/server.test.ts
+++ cli/tests/server.test.ts
@@ -1,6 +1,8 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import type { IRegistrationRequest } from "../src/types.ts";
 import { encode } from "https://deno.land/x/msgpack@v1.4/mod.ts";
+import { IKeyPair, deriveAuthKeyPair, generateSeed, sign } from "../src/auth.ts";
+import type { IOperationRequest, IRegistrationRequest } from "../src/types.ts";
+import { btoh } from "../src/lib.ts";
 
 function extractUrl(str: string): string | null {
   const urlRegex = /(https?:\/\/[^\s]+)/;
@@ -8,15 +10,21 @@ function extractUrl(str: string): string | null {
   return matches?.[0] ?? null;
 }
 
-const serverID = "id123";
+// Server config.
+const hostID = "id123";
 const registrationToken = "tok123";
+
+// Client config.
+const seed = generateSeed();
+const keyPair = deriveAuthKeyPair(hostID, seed);
+
 
 async function startServer(): Promise<{ proc: Deno.Process, url: URL } | undefined> {
   // NOTE: must run from cli dir.
   const p = Deno.run({
     cmd: ["deno", "run", "--allow-env", "--allow-net", "src/server.ts"],
     env: {
-      DIPLOMATIC_ID: serverID,
+      DIPLOMATIC_ID: hostID,
       DIPLOMATIC_REG_TOKEN: registrationToken,
     },
     stdout: "piped",
@@ -39,18 +47,15 @@ async function startServer(): Promise<{ proc: Deno.Process, url: URL } | undefin
   }
 }
 
-function testEndpoint(pathname: string, testFunc: (url: URL) => Promise<void>): () => Promise<void> {
+function testEndpoint(testFunc: (url: URL) => Promise<void>): () => Promise<void> {
   return async () => {
     const server = await startServer();
     if (!server) {
       throw "a fit";
     }
 
-    const url = server.url;
-    url.pathname = pathname;
-
     try {
-      await testFunc(url);
+      await testFunc(server.url);
     } finally {
       server.proc.close();
       server.proc.stdout?.close();
@@ -59,21 +64,57 @@ function testEndpoint(pathname: string, testFunc: (url: URL) => Promise<void>): 
   }
 }
 
-Deno.test("GET /id", testEndpoint("/id", async (url) => {
+Deno.test("GET /id", testEndpoint(async (url) => {
+  url.pathname = "/id";
   const response = await fetch(url, { method: "GET" });
   const body = await response.text();
-  assertEquals(body, serverID);
+  assertEquals(body, hostID);
 }));
 
+const pubKey = keyPair.publicKey;
 
-Deno.test("POST /users", testEndpoint("/users", async (url) => {
+Deno.test("POST /users", testEndpoint(async (url) => {
+  url.pathname = "/users";
   const req: IRegistrationRequest = {
     token: registrationToken,
-    pubKey: new Uint8Array([0xFF]), // TODO: check for valid pubKey length, server-side.
+    pubKey, // TODO: check for valid pubKey length, server-side.
   };
   const reqPack = encode(req);
 
   const response = await fetch(url, { method: "POST", body: reqPack });
+  await response.body?.cancel()
+  assertEquals(response.status, 200);
+}));
+
+Deno.test("POST /ops", testEndpoint(async (url) => {
+  const regReq: IRegistrationRequest = {
+    token: registrationToken,
+    pubKey, // TODO: check for valid pubKey length, server-side.
+  };
+  const regReqPack = encode(regReq);
+  url.pathname = "/users";
+  const regResp = await fetch(url, { method: "POST", body: regReqPack });
+  await regResp.body?.cancel();
+
+  const req: IOperationRequest = {
+    cipher: new Uint8Array([0xFE, 0xFE]),
+  };
+  const reqPack = encode(req);
+
+  const sig = sign(req.cipher, keyPair);
+  const sigHex = btoh(sig);
+  const keyHex = btoh(pubKey);
+
+  url.pathname = "/ops";
+  const response = await fetch(url, {
+    method: "POST", body: reqPack, headers: {
+      "X-DIPLOMATIC-SIG": sigHex,
+      "X-DIPLOMATIC-KEY": keyHex,
+    }
+  });
+  if (!response.ok) {
+    console.log(await response.text())
+  }
   await response.body?.cancel()
   assertEquals(response.status, 200);
 }));

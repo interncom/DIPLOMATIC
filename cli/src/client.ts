3c7f5b217fdebebaad43c2a59084bb5f2c7e1483
gr3ym4ge <mason@interncom.org>
2024-06-18 16:19:00 -0700

    make client class

--- cli/src/client.ts
+++ cli/src/client.ts
@@ -1,28 +1,108 @@
-import * as sodium from "https://raw.githubusercontent.com/interncom/libsodium.js/esm/dist/modules/libsodium-esm-wrappers.js";
-import { encode } from "https://deno.land/x/msgpack@v1.4/mod.ts";
+import { decode, encode } from "https://deno.land/x/msgpack@v1.4/mod.ts";
+import type { IOperationRequest, IRegistrationRequest } from "./types.ts";
+import { KeyPair, sign } from "./auth.ts";
+import { btoh } from "./lib.ts";
 
-export function serialize(data: unknown): Uint8Array {
-  return encode(data);
-}
+export class DiplomaticClient {
+  hostURL: URL;
+  constructor(hostURL: URL) {
+    this.hostURL = hostURL;
+  }
 
-export function encrypt(data: Uint8Array, key: Uint8Array): Uint8Array {
-  const { state } = sodium.crypto_secretstream_xchacha20poly1305_init_push(key) as { state: unknown };
-  const enc = sodium.crypto_secretstream_xchacha20poly1305_push(
-    state,
-    data,
-    null,
-    sodium.crypto_secretstream_xchacha20poly1305_TAG_FINAL,
-  ) as Uint8Array;
-  return enc;
-}
+  async getHostID(): Promise<string> {
+    const url = new URL(this.hostURL)
+    url.pathname = "/id";
+    const response = await fetch(url, { method: "GET" });
+    if (!response.ok) {
+      throw "Uh oh";
+    }
+    const id = await response.text();
+    return id;
+  }
 
-export function deriveEncryptionKey(seed: Uint8Array): Uint8Array {
-  const derivationIndex = 0;
-  const encKey = sodium.crypto_kdf_derive_from_key(
-    sodium.crypto_secretstream_xchacha20poly1305_KEYBYTES,
-    derivationIndex,
-    "encKey",
-    seed,
-  ) as Uint8Array;
-  return encKey;
+  async register(pubKey: Uint8Array, token: string): Promise<void> {
+    const url = new URL(this.hostURL)
+    url.pathname = "/users";
+    const req: IRegistrationRequest = {
+      token,
+      pubKey, // TODO: check for valid pubKey length, server-side.
+    };
+    const reqPack = encode(req);
+    const response = await fetch(url, { method: "POST", body: reqPack });
+    await response.body?.cancel()
+  }
+
+  async putDelta(cipherOp: Uint8Array, keyPair: KeyPair): Promise<string> {
+    const url = new URL(this.hostURL)
+    url.pathname = "/ops";
+
+    const req: IOperationRequest = {
+      cipher: cipherOp,
+    };
+    const reqPack = encode(req);
+
+    const sig = sign(req.cipher, keyPair);
+    const sigHex = btoh(sig);
+    const keyHex = btoh(keyPair.publicKey);
+
+    const response = await fetch(url, {
+      method: "POST", body: reqPack, headers: {
+        "X-DIPLOMATIC-SIG": sigHex,
+        "X-DIPLOMATIC-KEY": keyHex,
+      }
+    });
+    if (!response.ok) {
+      throw "Uh oh";
+    }
+    const opPath = await response.text();
+    return opPath;
+  }
+
+  async getDelta(opPath: string, keyPair: KeyPair): Promise<Uint8Array> {
+    const url = new URL(this.hostURL)
+    url.pathname = `/ops/${opPath}`;
+
+    const sig = sign(opPath, keyPair);
+    const sigHex = btoh(sig);
+    const keyHex = btoh(keyPair.publicKey);
+    const response = await fetch(url, {
+      method: "GET", headers: {
+        "X-DIPLOMATIC-SIG": sigHex,
+        "X-DIPLOMATIC-KEY": keyHex,
+      }
+    });
+    if (!response.ok) {
+      throw "Uh oh";
+    }
+    const respBuf = await response.arrayBuffer();
+    const resp = decode(respBuf) as { cipher: Uint8Array };
+    if (resp.cipher === undefined) {
+      throw "Missing cipher";
+    }
+    return resp.cipher;
+  }
+
+  async getDeltaPaths(begin: Date, keyPair: KeyPair): Promise<{ paths: string[] }> {
+    const t = begin.toISOString();
+    const path = `/ops?begin=${t}`;
+    const url = new URL(this.hostURL)
+    url.pathname = path;
+
+    const sigPath = `/ops%3Fbegin=${t}`;
+    const sig = sign(sigPath, keyPair);
+    const sigHex = btoh(sig);
+    const keyHex = btoh(keyPair.publicKey);
+    const response = await fetch(url, {
+      method: "GET", headers: {
+        "X-DIPLOMATIC-SIG": sigHex,
+        "X-DIPLOMATIC-KEY": keyHex,
+      }
+    });
+    if (!response.ok) {
+      throw "Uh oh";
+    }
+    const respBuf = await response.arrayBuffer();
+    const resp = decode(respBuf) as { paths: string[] };
+    return resp;
+  }
 }

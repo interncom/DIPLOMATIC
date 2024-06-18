7a044a4b41d9e55d5544f4d5a6f255aba58cc3f7
gr3ym4ge <mason@interncom.org>
2024-06-17 19:50:31 -0700

    refactor server tests

--- cli/tests/server.test.ts
+++ cli/tests/server.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { encode } from "https://deno.land/x/msgpack@v1.4/mod.ts";
-import { IKeyPair, deriveAuthKeyPair, generateSeed, sign } from "../src/auth.ts";
+import { deriveAuthKeyPair, generateSeed, sign } from "../src/auth.ts";
 import type { IOperationRequest, IRegistrationRequest } from "../src/types.ts";
 import { btoh } from "../src/lib.ts";
 
@@ -47,74 +47,60 @@ async function startServer(): Promise<{ proc: Deno.Process, url: URL } | undefin
   }
 }
 
-function testEndpoint(testFunc: (url: URL) => Promise<void>): () => Promise<void> {
-  return async () => {
-    const server = await startServer();
-    if (!server) {
-      throw "a fit";
-    }
-
-    try {
-      await testFunc(server.url);
-    } finally {
-      server.proc.close();
-      server.proc.stdout?.close();
-      server.proc.stderr?.close();
-    }
+Deno.test("server", async (t) => {
+  const server = await startServer();
+  if (!server) {
+    throw "a fit";
   }
-}
+  const url = server.url;
 
-Deno.test("GET /id", testEndpoint(async (url) => {
-  url.pathname = "/id";
-  const response = await fetch(url, { method: "GET" });
-  const body = await response.text();
-  assertEquals(body, hostID);
-}));
-
-const pubKey = keyPair.publicKey;
-
-Deno.test("POST /users", testEndpoint(async (url) => {
-  url.pathname = "/users";
-  const req: IRegistrationRequest = {
-    token: registrationToken,
-    pubKey, // TODO: check for valid pubKey length, server-side.
-  };
-  const reqPack = encode(req);
-
-  const response = await fetch(url, { method: "POST", body: reqPack });
-  await response.body?.cancel()
-  assertEquals(response.status, 200);
-}));
-
-Deno.test("POST /ops", testEndpoint(async (url) => {
-  const regReq: IRegistrationRequest = {
-    token: registrationToken,
-    pubKey, // TODO: check for valid pubKey length, server-side.
-  };
-  const regReqPack = encode(regReq);
-  url.pathname = "/users";
-  const regResp = await fetch(url, { method: "POST", body: regReqPack });
-  await regResp.body?.cancel();
-
-  const req: IOperationRequest = {
-    cipher: new Uint8Array([0xFE, 0xFE]),
-  };
-  const reqPack = encode(req);
-
-  const sig = sign(req.cipher, keyPair);
-  const sigHex = btoh(sig);
-  const keyHex = btoh(pubKey);
-
-  url.pathname = "/ops";
-  const response = await fetch(url, {
-    method: "POST", body: reqPack, headers: {
-      "X-DIPLOMATIC-SIG": sigHex,
-      "X-DIPLOMATIC-KEY": keyHex,
-    }
+  await t.step("GET /id", async () => {
+    url.pathname = "/id";
+    const response = await fetch(url, { method: "GET" });
+    const body = await response.text();
+    assertEquals(body, hostID);
   });
-  if (!response.ok) {
-    console.log(await response.text())
-  }
-  await response.body?.cancel()
-  assertEquals(response.status, 200);
-}));
+
+  const pubKey = keyPair.publicKey;
+
+  await t.step("POST /users", async () => {
+    url.pathname = "/users";
+    const req: IRegistrationRequest = {
+      token: registrationToken,
+      pubKey, // TODO: check for valid pubKey length, server-side.
+    };
+    const reqPack = encode(req);
+
+    const response = await fetch(url, { method: "POST", body: reqPack });
+    await response.body?.cancel()
+    assertEquals(response.status, 200);
+  });
+
+  await t.step("POST /ops", async () => {
+    const req: IOperationRequest = {
+      cipher: new Uint8Array([0xFE, 0xFE]),
+    };
+    const reqPack = encode(req);
+
+    const sig = sign(req.cipher, keyPair);
+    const sigHex = btoh(sig);
+    const keyHex = btoh(pubKey);
+
+    url.pathname = "/ops";
+    const response = await fetch(url, {
+      method: "POST", body: reqPack, headers: {
+        "X-DIPLOMATIC-SIG": sigHex,
+        "X-DIPLOMATIC-KEY": keyHex,
+      }
+    });
+    if (!response.ok) {
+      console.log(await response.text())
+    }
+    await response.body?.cancel()
+    assertEquals(response.status, 200);
+  });
+
+  server.proc.close();
+  server.proc.stdout?.close();
+  server.proc.stderr?.close();
+});

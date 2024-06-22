f99dd456fd98d30a735d253051037ffc6502c685
Mason <86958+masonicboom@users.noreply.github.com>
2024-06-21 22:45:49 -0700

    separate npm script for https server

--- web/vite.config.https.ts
+++ web/vite.config.https.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,10 @@ export default defineConfig({
   build: {
     target: 'es2022',
   },
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    },
+  }
 })

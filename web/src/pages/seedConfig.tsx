d6d0bce5b29078e1619a804a733d94c83ede0a2e
gr3ym4ge <mason@interncom.org>
2024-06-23 11:46:35 -0700

    bring back password save prompt

--- web/src/pages/seedConfig.tsx
+++ web/src/pages/seedConfig.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, FormEvent } from "react";
 import { btoh, htob } from "../../../cli/src/lib";
 import { generateSeed } from "../lib/auth";
 import type DiplomaticClient from "../lib/client";
@@ -16,18 +16,20 @@ export default function SeedConfig({ client }: IProps) {
     setSeedString(seedStr);
   }, []);
 
-  const handleInitFormSubmit = useCallback(() => {
+  const handleInitFormSubmit = useCallback((e: FormEvent) => {
     const seed = htob(seedString);
     client.setSeed(seed);
 
     // Trigger password save prompt.
     window.location.replace("/");
+
+    e.preventDefault();
   }, [seedString, client]);
 
   return (
     <div>
       <h1>Initialize</h1>
-      <form onSubmit={handleInitFormSubmit} style={{ display: "flex", flexDirection: "column" }}>
+      <form action="/" method="get" onSubmit={handleInitFormSubmit} style={{ display: "flex", flexDirection: "column" }}>
         <input name="username" type="text" autoComplete="username" placeholder={username} onChange={(e) => setUsername(e.target.value)} required />
         <input name="password" type="password" autoComplete="new-password" value={seedString} onChange={(e) => setSeedString(e.target.value)} />
         <button type="submit">Store</button>

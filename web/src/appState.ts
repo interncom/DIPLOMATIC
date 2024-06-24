1c2dc36dbc560cfb1daadf89b6215e082162a59c
gr3ym4ge <mason@interncom.org>
2024-06-24 13:48:37 -0700

    fix types and support async store init

--- web/src/appState.ts
+++ web/src/appState.ts
@@ -2,7 +2,7 @@ import { type IOp } from "../../cli/src/types";
 import * as Status from "./ops/status";
 
 // Applier MUST transactionally ignore deltas upserting entities modified after the delta timestamp.
-export function apply(op: IOp) {
+export async function apply(op: IOp) {
   const typeApplier = appliers[op.type];
   if (typeApplier?.typeChecks(op)) {
     return typeApplier.applier(op);

19bc283b4135e5d5affeb8f390c2b3a205b6f014
Mason <86958+masonicboom@users.noreply.github.com>
2024-06-24 22:02:02 -0700

    refactor server to use pluggable storage module

---
+++ hosts/deno-cli/server.ts
@@ -0,0 +1,30 @@
+import { DiplomaticServer } from "../../cli/src/server.ts";
+import memStorage from "../../cli/src/storage/memory.ts";
+
+const hostID = Deno.env.get("DIPLOMATIC_HOST_ID");
+const port = Number.parseInt(Deno.env.get("DIPLOMATIC_HOST_PORT"));
+const regToken = Deno.env.get("DIPLOMATIC_REG_TOKEN");
+if (!hostID) {
+  throw "Missing DIPLOMATIC_HOST_ID env var"
+}
+if (!port) {
+  throw "Missing DIPLOMATIC_HOST_PORT env var"
+}
+if (!regToken) {
+  throw "Missing DIPLOMATIC_REG_TOKEN env var"
+}
+
+const args = Deno.args;
+const useHttps = args.includes("--https");
+
+const server = new DiplomaticServer(hostID, regToken, memStorage);
+
+if (useHttps) {
+  const cert = Deno.readTextFileSync("../../certs/localhost.pem");
+  const key = Deno.readTextFileSync("../../certs/localhost-key.pem");
+  console.log(`DIPLOMATIC PARCEL SERVICE ACTIVE on https://localhost:${port}`);
+  Deno.serve({ port, cert, key }, server.corsHandler);
+} else {
+  console.log(`DIPLOMATIC PARCEL SERVICE ACTIVE on http://localhost:${port}`);
+  Deno.serve({ port }, server.corsHandler);
+}

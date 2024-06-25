fa649aa21399943607ce0dd1d8baf33e98670633
gr3ym4ge <mason@interncom.org>
2024-06-24 22:44:49 -0700

    abstract-out storage interface

--- cli/src/storage/memory.ts
+++ cli/src/storage/memory.ts
@@ -1,8 +1,39 @@
 import type { IStorage } from "../types.ts";
 
-const memStorage: IStorage = {
+interface IMemoryStorage extends IStorage {
+  users: Set<string>;
+  ops: Map<string, Uint8Array>;
+}
+const memStorage: IMemoryStorage = {
   users: new Set<string>(), // Set of user pubkeys in hex.
   ops: new Map(), // Path => op binary.
+
+  async addUser(pubKeyHex) {
+    this.users.add(pubKeyHex);
+  },
+
+  async hasUser(pubKeyHex) {
+    return this.users.has(pubKeyHex);
+  },
+
+  async setOp(path, op) {
+    this.ops.set(path, op);
+  },
+
+  async getOp(path) {
+    return this.ops.get(path);
+  },
+
+  async listOps(pubKeyHex, begin, end) {
+    const paths: string[] = [];
+    for (const [key, _] of this.ops) {
+      const [pkh, ts] = key.split('/');
+      if (pkh === pubKeyHex && ts >= begin && ts < end) {
+        paths.push(ts);
+      }
+    }
+    return paths;
+  },
 }
 
 export default memStorage;

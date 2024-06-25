f1aeb3413e7d1988285d7fb0ee3207e4d2f9add0
gr3ym4ge <mason@interncom.org>
2024-06-25 11:51:31 -0700

    fix import

--- web/src/lib/crypto.ts
+++ web/src/lib/crypto.ts
@@ -1,5 +1,5 @@
 // @ts-expect-error TODO: add type for sodium
-import * as sodium from "../../../cli/vendor/raw.githubusercontent.com/interncom/libsodium.js/esm/dist/modules/libsodium-esm-wrappers.js";
+import * as sodium from "../../../deno/vendor/raw.githubusercontent.com/interncom/libsodium.js/esm/dist/modules/libsodium-esm-wrappers.js";
 import { LibsodiumCrypto } from "../../../shared/crypto/libsodium.ts";
 
 const libsodiumCrypto = new LibsodiumCrypto(sodium);

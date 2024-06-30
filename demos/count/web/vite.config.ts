43e7ef417bf373389a6be0381cf855165c3861c7
gr3ym4ge <mason@interncom.org>
2024-06-29 19:28:19 -0700

    note on production build config for vite

--- demos/count/web/vite.config.ts
+++ demos/count/web/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2022',
+  },
+  // NOTE: this makes npm run dev work when including the DIPLOMATIC package from npm.
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2022',
+    },
+  },
 })

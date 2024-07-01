9c231d6db43df0e22d934a4b8cf459bcda8d8738
gr3ym4ge <mason@interncom.org>
2024-06-30 23:11:26 -0700

    queue pushes and pulls

---
+++ web/src/queue.ts
@@ -0,0 +1,31 @@
+import type { IQueue } from "./types";
+
+export class Queue<K, V> implements IQueue<K, V> {
+  private map: Map<K, V>;
+
+  constructor() {
+    this.map = new Map<K, V>();
+  }
+
+  async enqueue(key: K, value: V): Promise<void> {
+    this.map.set(key, value);
+  }
+
+  async peek(key: K): Promise<V | undefined> {
+    return this.map.get(key);
+  }
+
+  async entries(): Promise<Iterable<[K, V]>> {
+    return this.map.entries();
+  }
+
+  async dequeue(key: K): Promise<V | undefined> {
+    const value = this.map.get(key);
+    this.map.delete(key);
+    return value;
+  }
+
+  async size(): Promise<number> {
+    return this.map.size;
+  }
+}

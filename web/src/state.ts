f35ea6da5245bea932e75c64ed3531f492a4542b
gr3ym4ge <mason@interncom.org>
2024-07-02 14:04:50 -0700

    utilities to generate applier from map of op types

--- web/src/state.ts
+++ web/src/state.ts
@@ -40,3 +40,45 @@ export function useStateWatcher<T>(mgr: StateManager, opType: string, callback: 
   }, [mgr, opType, callback]);
   return val;
 }
+
+interface IApplier<T extends IOp> {
+  check: (op: IOp) => op is T;
+  apply: (op: T) => Promise<void>;
+}
+
+type Appliers<M> = {
+  [K in keyof M]: M[K] extends IOp ? IApplier<M[K]> : never;
+}
+
+// opMapApplier generates an operation applier from a record
+// mapping from op type to op IApplier. This is a convenience
+// function to organize handling of multiple op types.
+//
+// Example usage:
+//
+// export interface IStatusOp extends IOp {
+//   type: "status";
+//   body: string;
+// }
+// const applier = opMapApplier<{ status: IStatusOp }>({
+//   "status": {
+//     check: (op: IOp): op is IStatusOp => {
+//       return op.type === "status" && typeof op.body === "string";
+//     },
+//     apply: async (op: IStatusOp) => {
+//       const curr = statusStore.load();
+//       if (!curr?.updatedAt || op.ts > curr.updatedAt) {
+//         const status = op.body;
+//         statusStore.store({ status, updatedAt: op.ts });
+//       }
+//     }
+//   }
+// });
+export function opMapApplier<M>(appliers: Appliers<M>) {
+  return async (op: IOp) => {
+    const applier = appliers[op.type as keyof M];
+    if (applier?.check(op)) {
+      await applier.apply(op);
+    }
+  }
+}

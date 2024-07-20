58bd9aee46aacd64deeb4b6e8d84cc309f3d77a0
gr3ym4ge <mason@interncom.org>
2024-07-19 23:17:41 -0700

    make TODO app the default create template

--- create/templates/react-web/src/App.tsx
+++ create/templates/react-web/src/App.tsx
@@ -1,69 +1,47 @@
 import './App.css'
 import consts from './consts.json';
 import { useCallback, useState } from 'react';
-import { DiplomaticClient, idbStore, type IOp, opMapApplier, StateManager } from '@interncom/diplomatic'
+import { DiplomaticClient, htob, idbStore, EntityDB, btoh } from '@interncom/diplomatic'
 import { ClientStatusBar, InitSeedView, useStateWatcher, useClientState, useSyncOnResume } from '@interncom/diplomatic';
+import Todo from './Todo';
 
-interface IAppData {
-  val: string;
-  updatedAt: string;
+export interface ITodo {
+  text: string;
+  done?: boolean;
 }
+const opType = 'todo';
 
-const appStore = {
-  store(data: IAppData) {
-    localStorage.setItem("value", data.val);
-    localStorage.setItem("updatedAt", data.updatedAt);
-  },
-  load(): IAppData | undefined {
-    const val = localStorage.getItem("value") ?? undefined;
-    const updatedAt = localStorage.getItem("updatedAt") ?? undefined;
-    if (!val || !updatedAt) {
-      return undefined;
-    }
-    return { val, updatedAt };
-  },
-  async clear() {
-    localStorage.removeItem("value");
-  }
-}
-
-// Customize this to your app.
-export interface ICustomOp extends IOp {
-  type: "custom";
-  body: string;
-}
-
-const applier = opMapApplier<{ status: ICustomOp }>({
-  "custom": {
-    check: (op: IOp): op is ICustomOp => {
-      return op.type === "custom" && typeof op.body === "string";
-    },
-    apply: async (op: ICustomOp) => {
-      const curr = appStore.load();
-      if (!curr?.updatedAt || op.ts > curr.updatedAt) {
-        const val = op.body;
-        appStore.store({ val, updatedAt: op.ts });
-      }
-    }
-  }
-});
-const stateManager = new StateManager(applier, appStore.clear)
+const stateManager = EntityDB.stateManager;
 const client = new DiplomaticClient({ store: idbStore, stateManager });
 
 const hostURL = consts.hostURL;
+
+async function getTodos() {
+  return EntityDB.db.getAllFromIndex(EntityDB.entityTableName, EntityDB.typeIndexName, IDBKeyRange.only(opType));
+}
 
 export default function App() {
   useSyncOnResume(client);
   const state = useClientState(client);
   const link = useCallback(() => { client.registerAndConnect(hostURL) }, []);
 
-  const value = useStateWatcher(stateManager, "custom", appStore.load);
+  const todos = useStateWatcher(stateManager, opType, getTodos);
   const [valueField, setValueField] = useState("");
-  const handleSubmit = useCallback((evt: React.FormEvent) => {
+  const handleSubmit = useCallback(async (evt: React.FormEvent) => {
     evt.preventDefault();
-    client.upsert("custom", valueField);
+    const todo: ITodo = { text: valueField };
+    client.upsert<ITodo>(opType, todo);
     setValueField("");
   }, [valueField]);
+
+  const handleChange = useCallback(async (eid: string, text: string, done: boolean) => {
+    const todo: ITodo = { text, done };
+    client.upsert<ITodo>(opType, todo, htob(eid));
+  }, []);
+
+  const handleDelete = useCallback(async (eid: string) => {
+    client.delete(opType, htob(eid));
+  }, []);
 
   if (!client || !state) {
     return null;
@@ -74,11 +52,14 @@ export default function App() {
       <ClientStatusBar state={state} />
       {state.hasSeed ? (
         <>
-          <h1>APP</h1>
-          <div id="value">{value?.val}</div>
-          <div id="timestamp">{value?.updatedAt}</div>
-          <form onSubmit={handleSubmit}>
-            <input id="value-input" type="text" value={valueField} onChange={(evt) => setValueField(evt.target.value)} placeholder="Type a value ↵" />
+          <h1>TODO</h1>
+          {todos?.map((ent) => {
+            const todo = ent.body as ITodo;
+            const hex = btoh(ent.eid);
+            return <Todo key={hex} eid={hex} todo={todo} onChange={handleChange} onDelete={handleDelete} />;
+          })}
+          <form onSubmit={handleSubmit} style={{ marginBottom: 48, marginTop: 18 }}>
+            <input id="value-input" type="text" value={valueField} onChange={(evt) => setValueField(evt.target.value)} placeholder="Type a todo ↵" style={{ width: "100%", boxSizing: 'border-box', padding: 4 }} />
           </form>
           {
             state.hasHost

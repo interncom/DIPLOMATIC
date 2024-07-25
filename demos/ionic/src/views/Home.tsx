b15c5f414fa9ed45cc1d03f9be5f891ee2506966
Mason <86958+masonicboom@users.noreply.github.com>
2024-07-24 21:52:48 -0700

    ionic todo list demo

---
+++ demos/ionic/src/views/Home.tsx
@@ -0,0 +1,79 @@
+import { IonPage, IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonIcon, IonList, IonFooter, IonItem, IonInput, IonCheckbox } from "@ionic/react";
+import { useCallback, useState } from 'react';
+import { htob, EntityDB, btoh } from '@interncom/diplomatic'
+import { useStateWatcher, useClientState, useSyncOnResume } from '@interncom/diplomatic';
+import Todo from './Todo';
+import '../App.css'
+import { cogOutline } from "ionicons/icons";
+import { client, stateManager } from "../client";
+import ClientStatusBar from "./StatusBar";
+
+export interface ITodo {
+  text: string;
+  done?: boolean;
+}
+export const opType = 'todo';
+
+async function getTodos() {
+  return EntityDB.db.getAllFromIndex(EntityDB.entityTableName, EntityDB.typeIndexName, IDBKeyRange.only(opType));
+}
+
+export default function Home() {
+  useSyncOnResume(client);
+  const state = useClientState(client);
+
+  const todos = useStateWatcher(stateManager, opType, getTodos);
+  const [valueField, setValueField] = useState("");
+  const handleSubmit = useCallback(async (evt: React.FormEvent) => {
+    evt.preventDefault();
+    const todo: ITodo = { text: valueField };
+    client.upsert<ITodo>(opType, todo);
+    setValueField("");
+  }, [valueField]);
+
+  const handleChange = useCallback(async (eid: string, text: string, done: boolean) => {
+    const todo: ITodo = { text, done };
+    client.upsert<ITodo>(opType, todo, htob(eid));
+  }, []);
+
+  const handleDelete = useCallback(async (eid: string) => {
+    client.delete(opType, htob(eid));
+  }, []);
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="end">
+            <IonButton routerLink="/config"><IonIcon icon={cogOutline} /></IonButton>
+          </IonButtons>
+          <IonTitle>TODO</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonFooter>
+        <IonToolbar>
+          {state !== undefined ? <ClientStatusBar state={state} /> : undefined}
+        </IonToolbar>
+      </IonFooter>
+      <IonContent className="ion-padding">
+        {state?.hasSeed ? (
+          <>
+            <IonList>
+              {todos?.map((ent) => {
+                const todo = ent.body as ITodo;
+                const hex = btoh(ent.eid);
+                return <Todo key={hex} eid={hex} todo={todo} onChange={handleChange} onDelete={handleDelete} />;
+              })}
+              <form onSubmit={handleSubmit}>
+                <IonItem lines="full">
+                  <IonCheckbox slot="start" aria-label="Toggle completion" disabled checked={false} />
+                  <IonInput id="value-input" type="text" value={valueField} onIonInput={(evt) => setValueField(evt.target.value as string)} placeholder="Type a new todo ↵" style={{ width: "100%", boxSizing: 'border-box', padding: 4 }} />
+                </IonItem>
+              </form>
+            </IonList>
+          </>
+        ) : undefined}
+      </IonContent>
+    </IonPage>
+  );
+}
---
+++ demos/ionic/src/views/StatusBar.tsx
@@ -0,0 +1,42 @@
+import type { IDiplomaticClientState } from "../types";
+
+function GlowingDot({ on }: { on: boolean }) {
+  const color = on ? "#01FF70" : "#AAAAAA";
+  const size = 6;
+  return <div style={{ width: size, height: size, borderRadius: "50%", boxShadow: `0 0 4px ${color}`, backgroundColor: color }} />
+}
+
+function DotLabel({ on, label }: { on: boolean, label: string }) {
+  return (
+    <div style={{ display: "flex", flexDirection: "row", marginRight: 16, alignItems: "center", gap: 6 }}>
+      <GlowingDot on={on} />
+      {label}
+    </div >
+  )
+}
+
+interface IProps {
+  state: IDiplomaticClientState;
+}
+export default function ClientStatusBar({ state }: IProps) {
+  return (
+    <div
+      style={{
+        padding: "8px 16px",
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+      }}
+    >
+      <DotLabel on={state.hasSeed} label="SEED" />
+      <DotLabel on={state.hasHost} label="HOST" />
+      <DotLabel on={state.connected} label="LINK" />
+      <div style={{ marginRight: 16 }}>
+        ⇑ {state.numUploads}
+      </div>
+      <div>
+        ⇓ {state.numDownloads}
+      </div>
+    </div >
+  );
+}

be0176148d48102a13e2a756dbd2459e8fd2427d
Mason <86958+masonicboom@users.noreply.github.com>
2024-12-14 05:59:58 +0900

    fix typeIndexName to match new index

--- web/src/entityDB.ts
+++ web/src/entityDB.ts
@@ -1,10 +1,10 @@
-import { openDB, type DBSchema } from 'idb';
-import { Applier } from './types';
-import { IOp, Verb } from './shared/types';
-import { StateManager } from './state';
+import { type DBSchema, openDB } from "idb";
+import { Applier } from "./types";
+import { IOp, Verb } from "./shared/types";
+import { StateManager } from "./state";
 
-export const entityTableName = 'entities';
-export const typeIndexName = 'entity_type';
+export const entityTableName = "entities";
+export const typeIndexName = "entity_type_created_at";
 
 export interface IEntity<T> {
   eid: Uint8Array;
@@ -20,32 +20,46 @@ interface IEntityDB extends DBSchema {
     key: Uint8Array; // TODO: try Uint8Array;
     value: IEntity<unknown>;
     indexes: {
-      "entity_type_created_at": [string, Date],
+      "entity_type_created_at": [string, Date];
     };
-  }
+  };
 }
 
-export const db = await openDB<IEntityDB>('db', 6, {
+export const db = await openDB<IEntityDB>("db", 6, {
   upgrade(db) {
-    const store = db.createObjectStore('entities', { keyPath: 'eid', autoIncrement: false });
-    store.createIndex('entity_type_created_at', ['type', 'createdAt'], { unique: false });
-  }
+    const store = db.createObjectStore("entities", {
+      keyPath: "eid",
+      autoIncrement: false,
+    });
+    store.createIndex("entity_type_created_at", ["type", "createdAt"], {
+      unique: false,
+    });
+  },
 });
 
 export const applier: Applier = async (op: IOp) => {
   switch (op.verb) {
     case Verb.UPSERT: {
-      const curr = await db.get('entities', op.eid);
+      const curr = await db.get("entities", op.eid);
       if (new Date(op.ts) > (curr?.updatedAt ?? "")) {
-        await db.put('entities', { eid: op.eid, type: op.type, createdAt: curr?.createdAt ?? new Date(), updatedAt: new Date(op.ts), body: op.body });
+        await db.put("entities", {
+          eid: op.eid,
+          type: op.type,
+          createdAt: curr?.createdAt ?? new Date(),
+          updatedAt: new Date(op.ts),
+          body: op.body,
+        });
       }
       break;
     }
     case Verb.DELETE: {
-      await db.delete('entities', op.eid);
+      await db.delete("entities", op.eid);
       break;
     }
   }
 };
 
-export const stateManager = new StateManager(applier, () => db.clear('entities'));
+export const stateManager = new StateManager(
+  applier,
+  () => db.clear("entities"),
+);

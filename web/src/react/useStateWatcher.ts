d9b4fb293bce97e8da7f6153ffac943ad7d13247
gr3ym4ge <mason@interncom.org>
2025-04-27 21:46:58 -0400

    add suspense support

--- web/src/react/useStateWatcher.ts
+++ web/src/react/useStateWatcher.ts
@@ -1,7 +1,11 @@
-import { useState, useEffect } from "react";
+import { use, useEffect, useRef, useState } from "react";
 import type { StateManager } from "../state";
 
-export default function useStateWatcher<T>(mgr: StateManager, opType: string, callback: () => Promise<T>): T | undefined {
+export default function useStateWatcher<T>(
+  mgr: StateManager,
+  opType: string,
+  callback: () => Promise<T>,
+): T | undefined {
   const [val, setVal] = useState<T>();
   useEffect(() => {
     async function update() {
@@ -12,7 +16,27 @@ export default function useStateWatcher<T>(mgr: StateManager, opType: string, ca
     mgr.on(opType, update);
     return () => {
       mgr.off(opType, update);
-    }
+    };
   }, [mgr, opType, callback]);
   return val;
+}
+
+export function useStateWatcherSuspense<T>(
+  mgr: StateManager,
+  opType: string,
+  callback: () => Promise<T>,
+): T {
+  const promiseRef = useRef<Promise<T>>(callback());
+
+  useEffect(() => {
+    async function update() {
+      promiseRef.current = callback();
+    }
+    mgr.on(opType, update);
+    return () => {
+      mgr.off(opType, update);
+    };
+  }, [mgr, opType, callback]);
+
+  return use(promiseRef.current);
 }

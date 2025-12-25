60f2fb342bad79fdfed357ac6982e24f474c6268
masonicboom <mason@interncom.org>
2025-12-26 00:28:47 +0100

    refactor

--- shared/auth.ts
+++ shared/auth.ts
@@ -8,6 +8,9 @@ import {
 // timestampAuthProof authenticates with a sigproven timestamp.
 // The sigproof demonstrates control of the pubKey.
 // Host ientifies users by their pubkeys.
+// Server can reject for timestamp too far from its clock.
+// In that case, signal to user that clock is out of sync.
+// Clocks must be synchronized to ensure correct op order.
 export async function timestampAuthProof(
   derivationSeed: DerivationSeed,
   ts: Date,

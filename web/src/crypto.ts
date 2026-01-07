df0b5a20f28459958b8074d4e60cd92836b205a1
masonicboom <mason@interncom.org>
2026-01-07 14:43:54 +0100

    fix dependencies

--- web/src/crypto.ts
+++ web/src/crypto.ts
@@ -1,5 +1,5 @@
 // @ts-expect-error no type declaration
-import * as sodium from "../../deno/vendor/raw.githubusercontent.com/interncom/libsodium.js/esm/dist/modules/libsodium-esm-wrappers.js";
+import * as sodium from "libsodium.js/dist/modules/libsodium-esm-wrappers.js";
 import { LibsodiumCrypto } from "./shared/crypto/libsodium";
 
 const libsodiumCrypto = new LibsodiumCrypto(sodium);

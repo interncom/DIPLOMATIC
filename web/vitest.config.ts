06e96f274d5e1a135af23135c8534eb3f3b282f9
masonicboom <mason@interncom.org>
2026-01-13 23:32:37 +0100

    coverage reporting

---
+++ web/vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'json'],
+      include: ['src/**/*.ts'],
+      exclude: ['test/**/*.ts', 'dist/**', '**/*.d.ts'],
+    },
+  },
+})

91377e444b770c7d3288271d55e5a11c646e8e80
masonicboom <mason@interncom.org>
2026-01-19 11:49:57 +0100

    simplify valstat

--- shared/valstat.ts
+++ shared/valstat.ts
@@ -4,23 +4,12 @@ import { Status } from "./consts.ts";
 export type StatusOK = Status.Success;
 export type StatusNOK = Exclude<Status, Status.Success>;
 
-export type ValStat<T> =
-  | { val: T; status: StatusOK }
-  | { val: undefined; status: StatusNOK };
+export type ValStat<T> = [T, StatusOK] | [undefined, StatusNOK];
 
 export function ok<T>(val: T): ValStat<T> {
-  return { val, status: Status.Success };
+  return [val, Status.Success];
 }
 
 export function err<T>(stat: StatusNOK): ValStat<T> {
-  return { val: undefined, status: stat };
-}
-
-// vs is a helper that returns a ValStat as a [Val, Stat] tuple.
-export function vs<T>(valstat: ValStat<T>): [T, StatusOK] | [undefined, StatusNOK] {
-  const { val, status } = valstat;
-  if (status !== Status.Success) {
-    return [undefined, status];
-  }
-  return [val, status];
+  return [undefined, stat];
 }

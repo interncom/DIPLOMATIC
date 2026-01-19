40fe70169d63f93120684e24b789917881abdb92
masonicboom <mason@interncom.org>
2026-01-19 11:50:59 +0100

    simplify valstat

--- shared/valstat.ts
+++ shared/valstat.ts
@@ -4,7 +4,7 @@ import { Status } from "./consts.ts";
 export type StatusOK = Status.Success;
 export type StatusNOK = Exclude<Status, Status.Success>;
 
-export type ValStat<T> = [T, StatusOK] | [undefined, StatusNOK];
+export type ValStat<T> = [T, StatusOK] | [undefined, StatusNOK]
 
 export function ok<T>(val: T): ValStat<T> {
   return [val, Status.Success];

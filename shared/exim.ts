16ff0d948ae313d8a107ba156aa0edc4f51053eb
gr3ym4ge <mason@interncom.org>
2026-01-19 22:36:42 +0100

    document file format

--- shared/exim.ts
+++ shared/exim.ts
@@ -1,3 +1,46 @@
+import { ValStat } from "./valstat";
+
+/* File format
+
+Diplomatic files are a custom binary format designed to efficiently store bags.
+Bags are encrypted messages. Like with host-stored bags, the encryption key is
+derived from a string label and numerical index. Unlike with host-stored bags,
+in file-stored bags, the key label and index are stored alongside the bags, to
+minimize risk of lost keys.
+
+File contains sections:
+
+HEADER
+- key label
+- key index
+- bag count
+- blake3 hash of INDEX section
+
+INDEX
+- concatenated sequence of:
+  - head len
+  - headCph
+  - body len
+  - body offset
+
+BAGS
+- concatenated sequence of:
+  - bodyCph
+
+INDEX should be ordered last-first, to apply the latest data first.
+This allows obsolete messages to be skipped before application.
+
+The INDEX section allows parallelization for bag loading.
+
+File extension .dpl
+
+Should have a checksum of some sort in the HEADER, e.g. hash of concatenated bag hashes in the INDEX section.
+*/
+
+// export async function exportFile(name: string): ValStat<Uint8Array> {
+// }
+
+
 // Imports.
 // "file-saver": "^2.0.5",
 // "jszip": "^3.10.1",

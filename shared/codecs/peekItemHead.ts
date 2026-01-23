895afd33d342ab47188be1e3e0fd299215920b3f
gr3ym4ge <mason@interncom.org>
2026-01-23 11:14:11 +0100

    use varBytes for peekItem

--- shared/codecs/peekItemHead.ts
+++ shared/codecs/peekItemHead.ts
@@ -12,9 +12,8 @@ export const peekItemHeadCodec: ICodecStruct<IBagPeekItemHead> = {
   encode(enc, item): Status {
     enc.writeBytes(item.sig);
     enc.writeBytes(item.kdm);
-    const s = enc.writeVarInt(item.headCph.length);
+    const s = enc.writeVarBytes(item.headCph);
     if (s !== Status.Success) return s;
-    enc.writeBytes(item.headCph);
     return Status.Success;
   },
   decode(dec): ValStat<IBagPeekItemHead> {
@@ -22,9 +21,7 @@ export const peekItemHeadCodec: ICodecStruct<IBagPeekItemHead> = {
     if (s1 !== Status.Success) return err(s1);
     const [kdm, s2] = dec.readBytes(kdmBytes);
     if (s2 !== Status.Success) return err(s2);
-    const [headCphLen, s3] = dec.readVarInt();
-    if (s3 !== Status.Success) return err(s3);
-    const [headCph, s4] = dec.readBytes(headCphLen);
+    const [headCph, s4] = dec.readVarBytes();
     if (s4 !== Status.Success) return err(s4);
     return ok({ sig, kdm, headCph });
   },

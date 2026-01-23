cc8379441e22692a4d6e7adf73f36ba23a4ffe4c
masonicboom <mason@interncom.org>
2026-01-23 11:25:19 +0100

    use varBytes for bag codec (rearranges fields)

--- shared/codecs/bag.ts
+++ shared/codecs/bag.ts
@@ -7,12 +7,10 @@ export const bagCodec: ICodecStruct<IBag> = {
   encode(enc, bag): Status {
     enc.writeBytes(bag.sig);
     enc.writeBytes(bag.kdm);
-    const s1 = enc.writeVarInt(bag.headCph.length);
+    const s1 = enc.writeVarBytes(bag.headCph);
     if (s1 !== Status.Success) return s1;
-    const s2 = enc.writeVarInt(bag.bodyCph.length);
+    const s2 = enc.writeVarBytes(bag.bodyCph);
     if (s2 !== Status.Success) return s2;
-    enc.writeBytes(bag.headCph);
-    enc.writeBytes(bag.bodyCph);
     return Status.Success;
   },
   decode(dec) {
@@ -20,14 +18,10 @@ export const bagCodec: ICodecStruct<IBag> = {
     if (s1 !== Status.Success) return err(s1);
     const [kdm, s2] = dec.readBytes(kdmBytes);
     if (s2 !== Status.Success) return err(s2);
-    const [lenHeadCph, s3] = dec.readVarInt();
+    const [headCph, s3] = dec.readVarBytes();
     if (s3 !== Status.Success) return err(s3);
-    const [lenBodyCph, s4] = dec.readVarInt();
+    const [bodyCph, s4] = dec.readVarBytes();
     if (s4 !== Status.Success) return err(s4);
-    const [headCph, s5] = dec.readBytes(lenHeadCph);
-    if (s5 !== Status.Success) return err(s5);
-    const [bodyCph, s6] = dec.readBytes(lenBodyCph);
-    if (s6 !== Status.Success) return err(s6);
     return ok({
       sig,
       kdm,

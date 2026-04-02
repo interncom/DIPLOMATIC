d43f06081810a13feb55cd40d0e22c4a85412cc7
masonicboom <mason@interncom.org>
2026-04-02 10:58:43 +0200

    fix style bug

--- shared/crypto/libsodium.ts
+++ shared/crypto/libsodium.ts
@@ -1,6 +1,5 @@
 import type { DerivationSeed, Hash, ICrypto, KeyPair } from "../types.ts";
-// @ts-ignore
-import { blake3 } from "../../node_modules/@noble/hashes/blake3.js";
+import { blake3 } from "@noble/hashes/blake3.js";
 
 // @ts-ignore let this one go
 // deno-lint-ignore no-explicit-any

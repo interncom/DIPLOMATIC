cce6d6084a5f09a52f85eddb86a5ae19efece4e4
masonicboom <mason@interncom.org>
2026-04-11 16:01:40 +0200

    touchups

--- demos/status/plain/src/main.ts
+++ demos/status/plain/src/main.ts
@@ -1,28 +1,28 @@
 import { decode } from '@msgpack/msgpack'
 import * as Diplomatic from '@interncom/diplomatic'
 
+const msgType = "status";
+
 const statusDiv = document.getElementById('status')!
 
-export async function loadLatestStatus(seed: string, hostUrl: string): Promise<void> {
+export async function loadLatestStatus(seedHex: string, hostURL: string): Promise<void> {
   try {
-    const seedHex = seed.trim() || 'your-seed-here'
-    const hostURL = hostUrl.trim() || 'your-host-here'
-    if (!seedHex || seedHex === 'your-seed-here') throw new Error('Please enter a valid seed')
-    if (!hostURL || hostURL === 'your-host-here') throw new Error('Please enter a valid host URL')
-    const url = new URL(hostURL)
-    await Diplomatic.libsodiumCrypto.sodium.ready
-    const stateMgr = new Diplomatic.SingletonStateManager('status')
-    stateMgr.on("status", () => {
+    if (!seedHex) throw new Error('Please enter a valid seed')
+    if (!hostURL) throw new Error('Please enter a valid host URL')
+
+    const stateMgr = new Diplomatic.SingletonStateManager(msgType)
+    stateMgr.on(msgType, () => {
       if (!stateMgr.latest) return;
       const bodyStr = decode(stateMgr.latest) as string
       statusDiv.textContent = bodyStr
     })
-    const store = new Diplomatic.MemoryStore(Diplomatic.libsodiumCrypto)
-    const client = new Diplomatic.SyncClient(new Diplomatic.Clock(), stateMgr, store, (host) => new Diplomatic.HTTPTransport(host.handle))
 
-    const mseed = Diplomatic.htob(seedHex) as Diplomatic.MasterSeed
-    await client.setSeed(mseed)
-    await client.link({ handle: url, label: 'host' })
+    const store = new Diplomatic.MemoryStore<URL>(Diplomatic.libsodiumCrypto)
+    const client = new Diplomatic.SyncClient<URL>(new Diplomatic.Clock(), stateMgr, store, (host) => new Diplomatic.HTTPTransport(host.handle))
+
+    const seed = Diplomatic.htob(seedHex.trim()) as Diplomatic.MasterSeed
+    await client.setSeed(seed)
+    await client.link({ handle: new URL(hostURL), label: 'host' })
     await client.connect()
 
     const syncStatus = await client.sync()
